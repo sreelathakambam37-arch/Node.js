a32cf22c31a178521601e55eb267c8a3d0da27be
Kambam Sreelatha <sreelathakambam37@gmail.com>
2026-06-16 09:32:34 +0530

    post api

--- server.js
+++ server.js
@@ -3,9 +3,25 @@ const mongoose=require('mongoose');
 const UserData=require('./Model');
 const app=express();
 
+app.use(express.json());
 
+mongoose.connect('').then(
+    ()=>console.log('connected to database......')
+).catch(err=>console.log(err))
 
+app.post('/add_user', async (req,res)=>{
+    const {username}=req.body;
+    const {email}=req.body;
+    try{
+        const newData = new UserData({username,email});
+        await newData.save();
+        return res.json({message : "user data send successfully"});
+    }
+    catch(err){
+        console.log(err.message);
+    }
+})
 
-app.listen(2000,()=>console.log('server running.....okk'))
+app.listen(2000,()=>console.log('server running on http://127.0.0.1:2000......'))
 
 
