c88f7fe176e95e4e7d8183b4093348119ffea3d9
Kambam Sreelatha <sreelathakambam37@gmail.com>
2026-06-17 16:41:33 +0530

    done

--- server.js
+++ server.js
@@ -5,23 +5,35 @@ const app=express();
 
 app.use(express.json());
 
-mongoose.connect('').then(
+
+mongoose.connect('mongodb+srv://sreelathakambam37_db_user:sTXQ1enPc5YssUxO@cluster0.e4wcv65.mongodb.net/').then(
     ()=>console.log('connected to database......')
 ).catch(err=>console.log(err))
 
-app.post('/add_user', async (req,res)=>{
-    const {username}=req.body;
-    const {email}=req.body;
+app.post('/add_user', async (req,res)=>{ 
+    const {username} = req.body;
+    const {email} = req.body;
     try{
         const newData = new UserData({username,email});
-        await newData.save();
-        return res.json({message : "user data send successfully"});
+        awaitnewData.save();
+        return res.json({message: "user data send succesfully"});
+    }    
+    catch(err){
+        console.log(err.message);
+    }
+});
+
+app.put('/update/:id', async (req,res)=>{
+    const {username} = req.body;
+    const {email}  = req.body;
+    try{
+        await UserData.findByIdAndUpdate(req.params.id, {username,email},{new:true});
+        return res.json({message: "user data updated successfully"});
     }
     catch(err){
         console.log(err.message);
     }
-})
-
-app.listen(2000,()=>console.log('server running on http://127.0.0.1:2000......'))
+});
 
 
+app.listen(3000,()=>console.log('server running on http://127.0.0.1:3000.......'))
