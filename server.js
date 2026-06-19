c16520b08199b2ca238b1358f58f19525006d2b2
Kambam Sreelatha <sreelathakambam37@gmail.com>
2026-06-19 18:29:56 +0530

    done

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.post('/add_user', async (req,res)=>{
     const {email} = req.body;
     try{
         const newData = new UserData({username,email});
-        awaitnewData.save();
+        await newData.save();
         return res.json({message: "user data send succesfully"});
     }    
     catch(err){
@@ -27,7 +27,7 @@ app.put('/update/:id', async (req,res)=>{
     const {username} = req.body;
     const {email}  = req.body;
     try{
-        await UserData.findByIdAndUpdate(req.params.id, {username,email},{new:true});
+        await UserData.findByIdAndUpdate(req.params.id, {username,email},{returnDocument: "after"});
         return res.json({message: "user data updated successfully"});
     }
     catch(err){
@@ -35,6 +35,36 @@ app.put('/update/:id', async (req,res)=>{
     }
 });
 
+
+app.get('/get_all_data', async (req,res)=>{
+    try{
+        const allData = await UserData.find()
+        return res.json(allData)
+    }
+    catch(err){
+        console.log(err.message);
+    }
+
+})
+
+app.get('/get_data/:id', async (req,res)=>{
+    try{
+        const Data = await UserData.findById(req.params.id)
+        return res.json(Data)
+    }
+    catch(err){
+        console.log(err.message);
+    }
+})
+
+app.delete('/delete/:id', async (req,res)=>{
+    try{
+        await UserData.findByIdAndDelete(req.params.id);
+        return res.json({message: "user data deleted successfully"})
+    }
+    catch(err){
+        console.log(err.message);
+    }
 })
 
 app.listen(3000,()=>console.log('server running on http://127.0.0.1:3000......'))
